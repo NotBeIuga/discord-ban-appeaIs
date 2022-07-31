422b87d8f8e3eba31522168aaf5e3394700c2129
NotBeIuga <108774885+NotBeIuga@users.noreply.github.com>
2022-07-31 10:21:05 -0400

    Update unban.js

--- func/unban.js
+++ func/unban.js
@@ -17,7 +17,7 @@ export async function handler(event, context) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
+                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned please notify them in the appeals server.")}`
                     }
                 };
             } catch (e) {
